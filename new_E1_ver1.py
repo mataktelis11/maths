16bc2a0f8d1aa719952a8a64db59f8a816c8dbaf
mataktelis11 <61196956+mataktelis11@users.noreply.github.com>
2020-04-21 00:19:03 +0300

    Add files via upload

---
+++ new_E1_ver1.py
@@ -0,0 +1,320 @@
+###A.M.:P19100
+###Onoma:Aristotelis
+###Epwnimo:Matakias
+###Askisi E1 Efarmosmeni Algebra Ergasia I
+###PYTHON 3.8 
+
+"""
+Arxika pername ta moumera pou mas edwse o xristis se ena lexiko
+to opoio exoume arxikopoihsei ws 'permutation'
+
+Otan o xristis deinei ta noumera mporei na afisei osa kena thelei.
+An omws yparxoun mh eggyroi xaraktires ( xaraktires pou den einai akaireai ) to programma 
+den tous dexetai kai afinei ton xristi na 3anadokimasei
+
+Stin synexeia ginetai elegxos an einai pragmati metathesi me tin synartisi 'checkpermutation'
+h opoia elegxei an yparxoun epanali3eis h mh-thetikoi arithmoi kai epistrefei 'True' h 'False'
+
+Efwson h 'checkpermutation' epistre3ei 'True' to programma synexizei
+me tin synartisi 'printpermutation' h opoia dexetai san orisma to le3iko 'permutation' kai
+mia metavliti boolean tin 'Iperm' pou exoume arxikopoihsei ws 'True'
+
+H synartisi 'checkpermutation' ektipwnei tin metathesh kai to mikos ths kai elgexei
+ab h metathesh einai tautotikh kai stin periptwsh pou auto isxuei h 'Iperm' tha ginei 'True'
+alliws 'False'
+
+Akolouthei h  synatrisi'oppositeofpermutation' dexetai san orisma to le3iko 'permutation'
+kai ektipwnei tin antistrofh tis metathshs.
+
+Meta h synartisi 'findcycles' (dexetai san orisma to le3iko 'permutation') 
+topothetei sto keno array 'AllCycles' olous tous kyklous ths metatheshs
+
+H synartisi 'printallcycles' ektipwnei tin metathesei san ginomeno 
+olwn twn kwklwn
+
+H synartisi 'printstrangecycles' ektipwnei tin metathesei san ginomeno 
+kyklwn xwris tous kyklous me mhkos 1 
+
+Meta 'sarwnoume' mia fora to array 'AllCycles' gia na doume an yparxoun kykloi mhkous 1.
+An ypaxoun tote h boolean 'hasone'ginetai true alliws false
+
+An h metathesi einai tautotiki h exei mono ena stoixeio ( m=(1) ) h den exei kyklous mhkous 1
+tote kaleitai mono h synartisi 'printallcycles' alliws,
+kalountai kai oi 'printallcycles' kai 'printstrangecycles'.
+
+Telos kaleitai h synartisi 'countercycles':
+
+    Arxika elegxei an h metathesh exei toulaxiston 2 stoixeia (alliws aktypwnei oti den mporei h
+    metathesh na ekfrastei san ginomeno antimetathesewn)
+
+    H synartisi auti dexetai san orisma to array 'AllCycles' , tin boolean 'Iperm' kai to
+    keno array 'CounterCycles'
+
+    H synartisi gemiszei to array 'CounterCycles' me tis antimetatheseis ths metatheshs
+
+    An omws h metathesh einai tautotikh tote grafetai san ginomeno twn antimetathesewn:
+    (1,2)(1,2).
+
+    To sgn elegxeitai apo to mhkos tou array 'CounterCycles' me tin synartisi 'numberswitcher'
+
+"""
+
+
+
+def checkpermutation(permutation):
+
+    for i in permutation:
+        if permutation[i]>len(permutation):
+            print("this is not a permutation")
+            return False
+
+    for i in permutation:
+        s=0
+        for j in permutation:
+
+            if permutation[i]==permutation[j]:
+                s+=1
+                if s==2:
+
+                    print("this is not a permutation")
+                    return False
+                    
+
+    return True
+
+
+## Elegxei an o arithmos n einai artios h perritos allazontas n fores mia boolean metavliti
+## An exei idia timi me tin arxikh tha einai zigos alliws perritos
+def numberswitcher(n):
+    numberswitcher=True
+    for i in range(1,n+1):
+        if numberswitcher==True:
+            numberswitcher=False
+        else:
+            numberswitcher=True
+    return numberswitcher
+        
+
+def printpermutation(permutation,Iperm):
+
+    
+    print("Permutation is:")
+    print("m =",end=" ")
+    for i in range(1,len(permutation)+1):
+        print(permutation[i],end=" ")
+        if(i!=permutation[i]):
+            Iperm=False 
+    print()
+
+    
+    if(Iperm==True):
+        print("H metathesi einai tautotiki")
+    print("Lenght of permutation is:",len(permutation))
+
+    return Iperm
+
+def oppositeofpermutation(permutation):
+
+    print("Antistrofi tis metathesis:")
+    print("m^(-1) =",end=" ")
+
+    
+    for i in range(1,len(permutation)+1):
+        for j in range(1,len(permutation)+1):
+            if(permutation[j]==i):
+                print(j,end=" ")       
+    print()
+
+
+def countercycles(AllCycles,Iperm,CounterCycles):
+    if(len(permutation)<2):
+        print("Den mporei na ekfrastei san ginomeno antimetathesewn")
+    else:
+        
+        if (Iperm==False):
+            
+            for i in reversed(AllCycles):
+        
+                if len(i)==1:
+                    AllCycles.pop(AllCycles.index(i))
+                
+            element=[]
+            
+            for i in (range(len(AllCycles))):
+                if (len(AllCycles[i]))==2:
+                    CounterCycles.append(AllCycles[i])        
+                    
+                else:
+                    
+                    for j in range(1,len(AllCycles[i])):
+                        element.append(AllCycles[i][0])
+                        element.append(AllCycles[i][-1])
+                        CounterCycles.append(element)
+                        AllCycles[i].pop()
+                        element=[]
+                        
+        else:
+           CounterCycles=[[1,2],[1,2]] 
+
+
+        print("Metathesi san ginomeno antimetathesewn:")
+        print("m =",end=" ")
+        for i in range (len(CounterCycles)):
+            print("(",end=" ")
+            for j in range(len(CounterCycles[i])):
+                print(CounterCycles[i][j],end=" ")
+            print(")",end=" ")
+
+        print()
+        if (numberswitcher(len(CounterCycles))==False):
+            print("sgn(m)=-1 peritti")
+        else:
+            print("sgn(m)=1 artia")
+
+
+def findcycles(permutation):
+
+    permutationlen=len(permutation)
+
+    incycle=[False]*permutationlen
+
+
+
+    def adder():
+
+       while True:
+
+            if(permutation[cycle[-1]]!=cycle[0]):
+                incycle[cycle[-1]-1]=True
+                cycle.append(permutation[cycle[-1]])
+            else:
+                incycle[cycle[-1]-1]=True
+                break
+
+
+
+    def newlist(cycle):
+
+        for i in range(1,permutationlen+1):        
+            
+
+            if (i==permutationlen)and(incycle[-1]==True):
+            
+                cycle=[]
+                return cycle
+                    
+            elif(incycle[i-1]==False):
+
+                cycle=[i]
+                return cycle
+            else:
+                continue
+                    
+    cycle=[]
+    cycle.append(1)
+
+    while True:
+
+        adder()
+
+        AllCycles.append(cycle)
+
+        cycle=newlist(cycle)
+
+        if(len(cycle)==0):
+            break
+       
+    del cycle
+    #print(AllCycles)
+
+
+
+def printallcycles():
+
+    print("Metathesi san ginomeno 3enwn kyklwn:")
+    print("m =",end=" ")
+    
+    for i in range (len(AllCycles)):
+        
+        print("(",end=" ")
+        for j in range(len(AllCycles[i])):
+            print(AllCycles[i][j],end=" ")
+        print(")",end=" ")
+
+    print()
+
+def printstrangecycles():
+
+    print("Metathesi san ginomeno 3enwn kyklwn(xwris kyklous mhkous 1):")
+    print("m =",end=" ")
+    
+    for i in range (len(AllCycles)):
+        if(len(AllCycles[i])==1):
+            continue
+        print("(",end=" ")
+        for j in range(len(AllCycles[i])):
+            print(AllCycles[i][j],end=" ")
+        print(")",end=" ")
+
+    print()
+
+################################### M A I N () #######################################
+
+permutation={}
+index=1
+
+while True:
+    check=True
+    try:
+        a=input("Give a permutation:")
+        
+        a= a.split(" ")
+
+        for i in a:
+            if i ==(''):
+                continue
+            temp = int(i)
+            if (temp>0):
+                    
+                permutation[index]=temp
+                index += 1
+            else:
+                print("TYPED invalid:Only possitive integers")
+                check=False
+                break
+        if(check!=False):        
+            break
+    except ValueError:
+        print("TYPED invalid:Only numbers and spaces")
+        
+del index,temp
+
+if(checkpermutation(permutation)==True):
+    
+    AllCycles=[]
+    CounterCycles=[]
+    Iperm=True
+
+    Iperm=printpermutation(permutation,Iperm)
+
+    oppositeofpermutation(permutation)
+
+    findcycles(permutation)
+
+    hasone=False
+
+    for i in range(len(AllCycles)):
+        if len(AllCycles[i])==1:
+            hasone=True
+            break
+
+    if(Iperm==False) and (len(AllCycles)>1) and (hasone==True):
+        printallcycles()
+        printstrangecycles()
+    else:
+        printallcycles()   
+    
+    countercycles(AllCycles,Iperm,CounterCycles)
+
+
+
---
+++ new_E2_ver1.py
@@ -0,0 +1,99 @@
+###A.M.:P19100
+###Onoma:Aristotelis
+###Epwnimo:Matakias
+###Askisi E1 Efarmosmeni Algebra Ergasia I
+###PYTHON 3.8 
+
+"""
+Arxika ftiaxnoume tis synartiseis 'gcd' kai 'ekp' gia na vriskoume tin
+ta3i (order) mias metatheseis.
+
+H synartisi 'g(x)' dexetai san orisma ton arithmo pou edwse o xristis
+kai epistrefei tin megisti ta3i(order) gia tin opoia h metathesi einai
+ginomeno 2 3enwn kyklwn.
+
+Mesa stin 'g(x)' vrisketai h 'find_all_possible()' h opoia vriskei
+ola ta pithana zeurargia mhkwn 3enwn kyklwn. Gia kathe zeugari vriskei to ekp
+kai an einai megalytero tou 'max1' tote to 'max1' pairnei tin timi tou ekp.
+
+"""
+
+
+## Vriskoume to gcd me ton algorithmo tou Euklidi ##
+## Isxuei : gcd(a, b) = gcd(b, u), opou u to ypoloipo ths diairesis tou a me to b ##
+## Xrisimopoioume to mod (%) ##
+def gcd (a,b):   
+    if b==0:
+        return a
+    return gcd(b, a % b)
+## Me ton MKD eukola vriskoume to EKP ##
+def ekp (a,b):
+    return (a*b)/gcd (a,b)
+ 
+
+def g(x):
+
+    ##apantame amesws gia to 1 kai to 2 epeidi h 'find_all_possible'##
+    ##leitourgei gia arithmous megalyterous tou 3##
+   
+    if x==1:
+        print("Max order is 1")
+        return
+    if x==2:
+        #print("[1, 1]")
+        print("Max order is 1")
+        return
+
+    ## Ta zeuaria einai oi arithmoi 'a' kai 'b' kai exoun arxikes times ##
+    ## To 'a' au3anetai kata 1 - To 'b' meiwnetai kata 1##
+    a=1  
+    b=x-1        
+    max1=0
+    ## Gia na min pairnoume ta idia zeuragia 2 fores stamatame otan ta a kai b einai isa h otan a>b ##
+
+    def find_all_possible():
+
+        nonlocal max1
+            
+        nonlocal a
+
+        nonlocal b
+        
+        while True:
+
+            if a==x or a>b:
+                break   
+            #print(a,b)                
+            temp=int(ekp(a,b))
+
+            if (temp > max1):
+                max1=temp
+            del temp
+            if a==b :
+                break
+
+            a+=1
+            b-=1
+
+    find_all_possible()
+    
+    print ("Max order is",max1)
+    return
+
+###################################### M A I N ()#############################
+
+## Elegxoume oti deinei o xristis me vroxo pou stamataei na zhtaei eisodo mexri na dwthei ##
+## eggyrh eisodos (oxi charaktiras ,0 kai arnitikos arithmos) ##
+while True:
+    try:
+        a = int(input("Please enter a possitive number: "))
+        if a<=0:
+            print("Oops!  That was no valid number.  Try again...")
+            continue      
+        g(a)
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+
+
